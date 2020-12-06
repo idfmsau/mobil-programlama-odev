c1e280d80ab587c1991dedf7fa947ff0961f785a
Hilal İlayda Tezgider <ilayda.tezgider@hotmail.com>
2020-12-06 21:03:17 +0300

    MovieListScreen.js created and designed
    MovieListScreen.js created and designed.

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider} from '@ui-kitten/components';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen'
+import SignupScreen from './screens/SignupScreen';
+import MovieListScreen from './screens/MovieListScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+       <NavigationContainer>
+          <Stack.Navigator>
+          <Stack.Screen name="MovieListScreen" component={ MovieListScreen } />
+            {/* <Stack.Screen name="LoginScreen" component={ LoginScreen } />
+            <Stack.Screen name="SignupScreen" component={ SignupScreen } /> */}
+          </Stack.Navigator>
+        </NavigationContainer>
+    </ApplicationProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
